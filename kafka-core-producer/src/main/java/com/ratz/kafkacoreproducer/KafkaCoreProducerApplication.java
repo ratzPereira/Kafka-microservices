54e1b6e0e020cacda561fe385d131adb72ef7f4e
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2022-06-30 16:29:22 +0000

    built first basic consumer and producer

--- kafka-core-producer/src/main/java/com/ratz/kafkacoreproducer/KafkaCoreProducerApplication.java
+++ kafka-core-producer/src/main/java/com/ratz/kafkacoreproducer/KafkaCoreProducerApplication.java
@@ -1,13 +1,25 @@
 package com.ratz.kafkacoreproducer;
 
+import com.ratz.kafkacoreproducer.producer.HelloKafkaProducer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 @SpringBootApplication
-public class KafkaCoreProducerApplication {
+public class KafkaCoreProducerApplication implements CommandLineRunner {
+
+	@Autowired
+	private HelloKafkaProducer producer;
 
 	public static void main(String[] args) {
 		SpringApplication.run(KafkaCoreProducerApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		producer.sendHello("Ratz" + ThreadLocalRandom.current().nextInt());
+	}
 }
