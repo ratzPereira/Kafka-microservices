f84c5cbdd2c7f1b41a9eac12ecb851779653668c
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2022-06-30 16:42:11 +0000

    created another producer and consumer to see messages in real time being produced and consumed

--- kafka-core-producer/src/main/java/com/ratz/kafkacoreproducer/KafkaCoreProducerApplication.java
+++ kafka-core-producer/src/main/java/com/ratz/kafkacoreproducer/KafkaCoreProducerApplication.java
@@ -5,14 +5,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.util.concurrent.ThreadLocalRandom;
 
 @SpringBootApplication
+@EnableScheduling
 public class KafkaCoreProducerApplication implements CommandLineRunner {
 
-	@Autowired
-	private HelloKafkaProducer producer;
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(KafkaCoreProducerApplication.class, args);
@@ -20,6 +21,7 @@ public class KafkaCoreProducerApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		producer.sendHello("Ratz" + ThreadLocalRandom.current().nextInt());
+
+
 	}
 }
---
+++ kafka-core-producer/src/main/java/com/ratz/kafkacoreproducer/producer/FixedRateProducer.java
@@ -0,0 +1,28 @@
+package com.ratz.kafkacoreproducer.producer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Service
+public class FixedRateProducer {
+
+  private static final Logger log = LoggerFactory.getLogger(FixedRateProducer.class.getSimpleName());
+
+  @Autowired
+  KafkaTemplate<String, String> kafkaTemplate;
+
+  private AtomicInteger counter = new AtomicInteger();
+
+  @Scheduled(fixedRate = 1000)
+  public void sendMessage(){
+    int i = counter.decrementAndGet();
+    log.info("I is " + i);
+    kafkaTemplate.send("t-fixedrate", "Fixed Rate " + i);
+  }
+}
